f1012ac3509bd824fa4e0ef9f3acdb5f10526f31
Gustave C <gustave.caillaut@gmail.com>
2025-10-15 15:43:31 +0200

    test_init

---
+++ The_Alchesmith/Source/The_Alchesmith/The_Alchesmith.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "The_Alchesmith.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, The_Alchesmith, "The_Alchesmith" );
