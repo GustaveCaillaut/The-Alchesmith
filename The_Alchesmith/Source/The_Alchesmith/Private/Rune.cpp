90bd6d7c8c76c5e40d0b1f0967ee01ec870fc769
SebastianPartarrieu <sebastian.partarrieu@mines-paristech.fr>
2025-10-20 15:16:09 +0200

    Changes to rune cpp files

--- The_Alchesmith/Source/The_Alchesmith/Private/Rune.cpp
+++ The_Alchesmith/Source/The_Alchesmith/Private/Rune.cpp
@@ -3,40 +3,54 @@
 
 #include "Rune.h"
 
+static const int32 N = 5;
+static const int32 FusionMatrix[N * N] = {
+	0, 0, 0, 0, 0, // Row 0
+	1, 0, 4, 0, 0, // Row 1
+	2, 4, 0, 0, 0, // Row 2
+	3, 0, 0, 0, 0, // Row 3
+	4, 0, 0, 0, 0  // Row 4
+};
+int32 URune::GetFusionID(ERuneID j) {
+	int32 old_id = static_cast<int32>(this->id);
+	int32 new_id = static_cast<int32>(j);
+	return FusionMatrix[old_id * N + new_id];
+};
+
 void URune::Init(ERuneID idToSet) {
-	
+
 	bool temp[4] = { true, false, false, true };
 
 	switch (idToSet) {
 
-		case ERuneID::FIRE: 
-			this->id = idToSet;
-			this->drawColor = FColor::Red;
-			this->drawing.Append(temp, 4);
+	case ERuneID::FIRE:
+		this->id = idToSet;
+		this->drawColor = FColor::Red;
+		this->drawing.Append(temp, 4);
 
-			break;
+		break;
 
-		case ERuneID::AIR:
-			this->id = idToSet;
-			this->drawColor = FColor(128, 128, 128, 255); //XXX
-			this->drawing.Append(temp, 4);
-			break;
-	
-
-		case ERuneID::EARTH:
-			this->id = idToSet;
-			this->drawColor = FColor::Cyan; //XXX
-			this->drawing.Append(temp, 4);
-			
-			break;
+	case ERuneID::AIR:
+		this->id = idToSet;
+		this->drawColor = FColor(128, 128, 128, 255); //XXX
+		this->drawing.Append(temp, 4);
+		break;
 
 
-		case ERuneID::LIGHTNING:
-			this->id = idToSet;
-			this->drawColor = FColor::Yellow;
-			this->drawing.Append(temp, 4);
-			
-			break;
+	case ERuneID::EARTH:
+		this->id = idToSet;
+		this->drawColor = FColor::Cyan; //XXX
+		this->drawing.Append(temp, 4);
+
+		break;
+
+
+	case ERuneID::LIGHTNING:
+		this->id = idToSet;
+		this->drawColor = FColor::Yellow;
+		this->drawing.Append(temp, 4);
+
+		break;
 	}
 
 }
